7d12df4df0c4871bcbb91c5419e862fda7f3212f
robiulislam12 <therobiulislam12@gmail.com>
2021-11-10 03:30:35 +0600

    Hero Section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Box, Button, Container, Divider, Typography } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 export default function Footer() {
     return (
-        <div>
-            <h1>Footer Section</h1>
-        </div>
+        <Box sx={{backgroundColor:'#fff', py:3,}}>
+            <Container>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Box>
+                    <Link to='/'>
+                            <img style={{width: '200px'}} src={logo} alt="" className="logo" />
+                        </Link>
+                    </Box>
+                    <Box>
+                        <Link to='/home'><Button>Home</Button></Link>
+                        <Link to='/cars'><Button>Cars</Button></Link>
+                    </Box>
+                </Box> 
+                <Divider sx={{my:2}}/>
+                <Typography align='center'>2021 MetroCars. All Rights reserved</Typography>
+            </Container>
+        </Box>
     )
 }
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Box, Container, Grid } from '@mui/material';
+import React from 'react';
+
 
 export default function HeroSection() {
     return (
-        <div>
-            <h1>Hero Section</h1>
-        </div>
+        <Box>
+            <Container>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} md={6}></Grid>
+                    <Grid item xs={12} md={6}></Grid>
+                </Grid>
+            </Container>
+        </Box>
     )
 }
